332675bc5f364bf25f087705a84f22c540624294
asheq <Asheq096@gmail.com>
2018-02-22 23:48:12 -0500

    Updated all packages. Added models project with some empty models.

---
+++ Crypt0Tracker.Models/Holding.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Crypt0Tracker.Models
+{
+    class Holding
+    {
+        [StringLength(10)]
+        public string Ticker { get; set; }
+    }
+}
